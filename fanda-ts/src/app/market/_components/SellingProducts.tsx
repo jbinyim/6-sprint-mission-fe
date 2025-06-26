5323fce98b01142a473110e5478f2ec4231bd098
jbinyim <jbinyim12@naver.com>
2025-06-26 15:16:27 +0900

    feat: market 디자인 완료

--- fanda-ts/src/app/market/_components/SellingProducts.tsx
+++ fanda-ts/src/app/market/_components/SellingProducts.tsx
@@ -5,9 +5,31 @@ import FilterDropDown from "@/components/common/dropDowns/FilterDropDown";
 import { useResponsive } from "@/utils/response";
 import React, { useState } from "react";
 
+const dummyCards = [
+  { id: 1, title: "Card 1" },
+  { id: 2, title: "Card 2" },
+  { id: 3, title: "Card 3" },
+  { id: 4, title: "Card 4" },
+  { id: 5, title: "Card 5" },
+  { id: 6, title: "Card 6" },
+  { id: 7, title: "Card 6" },
+  { id: 8, title: "Card 6" },
+  { id: 9, title: "Card 6" },
+  { id: 10, title: "Card 6" },
+  { id: 11, title: "Card 6" },
+];
+
 const SellingProducts = () => {
   const [isFilter, setIsFilter] = useState<boolean>(false);
+  const [filterMenu, setFilterMenu] = useState<string>("최신순");
+
   const { isMobile, isTablet, isPc } = useResponsive();
+
+  let visibleCardCount = 4;
+  if (isTablet) visibleCardCount = 6;
+  if (isPc) visibleCardCount = 10;
+
+  const cardsToRender = dummyCards.slice(0, visibleCardCount);
 
   return (
     <div>
@@ -36,7 +58,9 @@ const SellingProducts = () => {
                   className="w-[24px] h-[24px]"
                 />
               </div>
-              {isFilter && <FilterDropDown right="0px" />}
+              {isFilter && (
+                <FilterDropDown right="0px" setFilterMenu={setFilterMenu} />
+              )}
             </span>
           </form>
         </nav>
@@ -58,14 +82,66 @@ const SellingProducts = () => {
               className="cursor-pointer relative"
             >
               <div className="flex items-center justify-center gap-[24px] w-[130px] h-[42px] border border-gray200 rounded-[12px]">
-                <p>최신순</p>
+                <p>{filterMenu}</p>
                 <img src="/icons/ic_arrow_down.png" alt="드롭다운 버튼" />
               </div>
-              {isFilter && <FilterDropDown right="0px" top="50px" />}
+              {isFilter && (
+                <FilterDropDown
+                  right="0px"
+                  top="50px"
+                  setFilterMenu={setFilterMenu}
+                />
+              )}
             </span>
           </form>
         </nav>
       ) : null}
+      <section className="mt-[16px] mb-[40px] grid grid-cols-[repeat(2,_166px)] items-center justify-center gap-x-[8px] gap-y-[32px] tablet:grid-cols-[repeat(3,_220px)] tablet:gap-x-[16px] tablet:gap-y-[40px] pc:grid-cols-[repeat(5,_220px)] pc:gap-x-[24px]">
+        {cardsToRender.map((card) => (
+          <div key={card.id} className="w-[166px] tablet:w-[220px]">
+            <img
+              src="/markets/productLoding.png"
+              alt="상품 이미지"
+              className="h-[166px] rounded-[9px] tablet:h-[220px]"
+            />
+            <div className="mt-[16px] flex flex-col gap-[6px]">
+              <p className="text-[14px] font-[500] text-gray800">로봇 청소기</p>
+              <p className="font-bold text-gray800">1,500,000원</p>
+              <div className="flex items-center gap-[4px]">
+                <img
+                  src="/icons/ic_heart.png"
+                  alt="좋아요 수"
+                  className="w-[16px] h-[16px]"
+                />
+                <p className="text-[12 px] font-[500] text-gray600">240</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </section>
+      <section className="w-[304px] mx-auto flex items-center gap-[4px]">
+        <div className="w-[40px] h-[40px] border border-gray200 rounded-[40px] flex items-center justify-center text-gray600 cursor-pointer">
+          {"<"}
+        </div>
+        <div className="w-[40px] h-[40px] border border-gray200 rounded-[40px] flex items-center justify-center cursor-pointer bg-primary200 text-white">
+          1
+        </div>
+        <div className="w-[40px] h-[40px] border border-gray200 rounded-[40px] flex items-center justify-center text-gray500 cursor-pointer">
+          2
+        </div>
+        <div className="w-[40px] h-[40px] border border-gray200 rounded-[40px] flex items-center justify-center text-gray500 cursor-pointer">
+          3
+        </div>
+        <div className="w-[40px] h-[40px] border border-gray200 rounded-[40px] flex items-center justify-center text-gray500 cursor-pointer">
+          4
+        </div>
+        <div className="w-[40px] h-[40px] border border-gray200 rounded-[40px] flex items-center justify-center text-gray500 cursor-pointer">
+          5
+        </div>
+        <div className="w-[40px] h-[40px] border border-gray200 rounded-[40px] flex items-center justify-center text-gray600 cursor-pointer">
+          {">"}
+        </div>
+      </section>
     </div>
   );
 };
--- fanda-ts/src/components/common/dropDowns/FilterDropDown.tsx
+++ fanda-ts/src/components/common/dropDowns/FilterDropDown.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 
 interface FilterDropDownProps {
@@ -5,16 +6,30 @@ interface FilterDropDownProps {
   right?: string;
   left?: string;
   bottom?: string;
+  setFilterMenu: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const FilterDropDown = ({ top, right, left, bottom }: FilterDropDownProps) => {
+const FilterDropDown = ({
+  top,
+  right,
+  left,
+  bottom,
+  setFilterMenu,
+}: FilterDropDownProps) => {
   return (
     <ul
       style={{ top, bottom, right, left }}
       className="absolute w-[130px] h-[84px] bg-white rounded-[12px] border border-gray200 text-center leading-[42px] text-gray800"
     >
-      <li className="h-[50%] border-b border-gray200 cursor-pointer">최신순</li>
-      <li className="cursor-pointer">좋아요순</li>
+      <li
+        className="h-[50%] border-b border-gray200 cursor-pointer"
+        onClick={() => setFilterMenu("최신순")}
+      >
+        최신순
+      </li>
+      <li className="cursor-pointer" onClick={() => setFilterMenu("좋아요순")}>
+        좋아요순
+      </li>
     </ul>
   );
 };
